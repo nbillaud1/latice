f1aeecd84039b14a34de234ca330a36f697ac5f2
Nathan Billaud <natbil@orange.fr>
2025-04-01 15:06:12 +0200

    commit Développé par Nathan

--- src/main/java/latice/application/LacticeApplicationConsole.java
+++ src/main/java/latice/application/LacticeApplicationConsole.java
@@ -8,7 +8,8 @@ public class LacticeApplicationConsole {
 		System.out.println(LIGNE);
 		System.out.println("  --  Bienvenue dans notre magnifique jeu de latice !  --  ");
 		System.out.println("  --      	   Développé par Reia !             --  ");		
-		System.out.println("  --  		   Dévelopé par Louis !  	       --  ");
+		System.out.println("  --  		   Dévelopé par Louis !  	        --  ");
+		System.out.println("  --  		   Dévelopé par Nathan !  	        --  ");
 		System.out.println(LIGNE);
 	}
 
