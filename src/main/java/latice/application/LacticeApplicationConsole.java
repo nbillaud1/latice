4aa2adcccab8f7c00a57537adc907143de046dba
Nathan Billaud <natbil@orange.fr>
2025-04-01 14:20:20 +0200

    ajout LaticeApplicationConsole

---
+++ src/main/java/latice/application/LacticeApplicationConsole.java
@@ -0,0 +1,13 @@
+package latice.application;
+
+public class LacticeApplicationConsole {
+
+	private final static String LIGNE = "------------------------------------------------------------";
+	
+	public static void main(String[] args) {
+		System.out.println(LIGNE);
+		System.out.println("  --  Bienvenue dans notre magnifique jeu de latice !  --  ");
+		System.out.println(LIGNE);
+	}
+
+}
