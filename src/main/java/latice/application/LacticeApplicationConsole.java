d9bf4cc0de5a93381914df2886e3c66a832fa42c
Louis Biard <louisbiard24@gmail.com>
2025-04-01 15:04:54 +0200

    Développé par Louis

--- src/main/java/latice/application/LacticeApplicationConsole.java
+++ src/main/java/latice/application/LacticeApplicationConsole.java
@@ -7,7 +7,8 @@ public class LacticeApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println(LIGNE);
 		System.out.println("  --  Bienvenue dans notre magnifique jeu de latice !  --  ");
-		System.out.println("  --      Développé par Reia      --  ");		
+		System.out.println("  --      	   Développé par Reia !             --  ");		
+		System.out.println("  --  		   Dévelopé par Louis !  	       --  ");
 		System.out.println(LIGNE);
 	}
 
