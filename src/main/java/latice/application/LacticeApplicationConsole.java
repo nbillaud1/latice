fb0c8a517ef1adb9e2f3f379c9ccb3d5bf6a6f1e
Reia Taero <reiahoani.taero@etu.unilim.fr>
2025-04-11 10:19:00 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/latice/application/LacticeApplicationConsole.java
+++ src/main/java/latice/application/LacticeApplicationConsole.java
@@ -7,11 +7,17 @@ public class LacticeApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println(LIGNE);
 		System.out.println("  --  Bienvenue dans notre magnifique jeu de latice !  --  ");
+<<<<<<< Updated upstream
 		System.out.println("  --           Développé par Reia !                    --  ");		
 		System.out.println("  --  		   Développé par Louis !  	        --  ");
 		System.out.println("  --  		   Développé par Nathan !  	        --  ");
 		System.out.println("  --  		   Développé par Sami !  	        --  ");
 		System.out.println("  --  		   Développé par Evan!  	        --  ");
+=======
+		System.out.println("  --      	   Développé par Reia !             --  ");		
+		System.out.println("  --  		   Développé par Louis !  	        --  ");
+		System.out.println("  --  		   Développé par Nathan !  	        --  ");
+>>>>>>> Stashed changes
 		System.out.println(LIGNE);
 	}
 
