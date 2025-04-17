5507f3ef4ebedb33e572b3349dbf11dc50c1c955
Nathan Billaud <natbil@orange.fr>
2025-04-17 08:51:33 +0200

    fin de rack et pool

--- src/main/java/latice/model/Rack.java
+++ src/main/java/latice/model/Rack.java
@@ -9,12 +9,11 @@ public class Rack {
 	public Rack() {
 		this.pool = new Pool();
 		this.tiles = new ArrayList<Tile>();
-		/*this.tiles.add(this.pool.tiles()[0]);
-		this.tiles.add(this.pool.tiles()[1]);
-		this.tiles.add(this.pool.tiles()[2]);
-		this.tiles.add(this.pool.tiles()[3]);
-		this.tiles.add(this.pool.tiles()[4]);*/
-		//TODO
+		this.tiles.add(this.pool.tiles().get(0));
+		this.tiles.add(this.pool.tiles().get(1));
+		this.tiles.add(this.pool.tiles().get(2));
+		this.tiles.add(this.pool.tiles().get(3));
+		this.tiles.add(this.pool.tiles().get(4));
 		this.pool.tiles().remove(0);
 		this.pool.tiles().remove(1);
 		this.pool.tiles().remove(2);
