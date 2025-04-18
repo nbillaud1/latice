bc6145389d190f83cfb3544dd03458fe2706bb72
Nathan Billaud <natbil@orange.fr>
2025-04-18 17:09:29 +0200

    vrai ajout de SceneBuilder (test interface graphique)

---
+++ src/main/java/latice/controller/GameViewController.java
@@ -0,0 +1,22 @@
+package latice.controller;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class GameViewController implements EventHandler<MouseEvent>{
+	
+	@FXML
+	private Label idBjr;
+	
+	@Override
+	public void handle(MouseEvent event) {
+		//TODO
+    }
+
+    @FXML
+    void initialize() {
+    	//TODO
+    }
+}
