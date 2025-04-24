18096aee5057568df51de67e7ec8d4f48ff8b191
Louis Biard <louisbiard24@gmail.com>
2025-04-24 18:05:30 +0200

    Ajout de la grille (sans soleils/lunes) dans LaticeAppSb

--- src/main/java/latice/LaticeAppSb.java
+++ src/main/java/latice/LaticeAppSb.java
@@ -2,26 +2,48 @@ package latice;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class LaticeAppSb extends Application{
+	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("view/GameView.fxml"));
 		
-		/* Mise en place de la page */
-		StackPane root = new StackPane();
-		root.getChildren().add(loader.load());
+		/*Création de la grille*/
+		GridPane grille = new GridPane();
+		grille.setHgap(0.2);
+        grille.setVgap(0.2);
+        grille.setPadding(new Insets(450));
+        
+  
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+            	Rectangle square = new Rectangle(50, 50, Color.WHITE);
+                square.setStroke(Color.BLACK);
+                grille.add(square, col, row);
+            }
+        }
+
 		
-		Scene scene = new Scene(root,1000,1000);
+		/* Mise en place de la page */
+		
+        StackPane root = new StackPane();
+        root.getChildren().add(grille);
+		Scene scene = new Scene(root,1200,900);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Jeu du Latice");
 		primaryStage.show();
 	}
+	
 
 	public static void main(String[] args) {
 		Application.launch(args);
