c268119a18aeae185e352f173fcbe7b4a59aced1
Louis Biard <louisbiard24@gmail.com>
2025-05-07 10:52:59 +0200

    Ajout création de Player et TODO de: buyExtraMove(), playATile(), chagerRackAndPass(), pass(), completeRack(), setPoints()

--- src/main/java/latice/model/Player.java
+++ src/main/java/latice/model/Player.java
@@ -1,5 +1,42 @@
 package latice.model;
 
-public class Player {
+import latice.model.Pool;
 
+public class Player {
+	private Pool pool;
+	private Rack rack;
+	private int points;
+	private String name;
+	
+	public Player(Pool pool, Rack rack, String name /*on choisira "1" ou "2" */) {
+		this.pool = pool;
+		this.rack = rack;
+		this.name = name;
+		this.points = 0;
+		
+	}
+	
+	public buyExtraMove(){
+		//TODO
+	}
+	
+	public playATile() {
+		//TODO
+	}
+	
+	public chagerRackAndPass() {
+		//TODO
+	}
+	
+	public pass() {
+		//TODO
+	}
+	
+	public completeRack() {
+		//TODO
+	}
+	
+	public setPoints(int points) {
+		//TODO
+	}
 }
