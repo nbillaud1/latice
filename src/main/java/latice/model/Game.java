6ed2fa3e19ed64a50bfa81aab22f529f80811c66
Louis Biard <louisbiard24@gmail.com>
2025-05-07 16:38:13 +0200

    Ajout préliminaire de player1 et player2

--- src/main/java/latice/model/Game.java
+++ src/main/java/latice/model/Game.java
@@ -5,11 +5,14 @@ import java.util.Collections;
 
 public class Game {
 	//private GameBoard gameBoard;
-	//private ArrayList<Player> players;
+	private Player player1;
+	private Player player2;
 	private MainPool mainPool;
 	
 	public Game() {
 		this.mainPool = new MainPool();
+		//this.player1 = new Player(pool,rack,"1");
+		//this.player2 = new Player(pool,rack,"2");
 	}
 	
 	public MainPool mainPool() {
