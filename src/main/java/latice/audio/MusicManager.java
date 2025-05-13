2e2fce99c91f7536d035374bc405309b36acfbbf
Louis Biard <louisbiard24@gmail.com>
2025-05-13 14:35:11 +0200

    Ajout des music et de MusicManager

---
+++ src/main/java/latice/audio/MusicManager.java
@@ -0,0 +1,32 @@
+package latice.audio;
+
+import java.net.URL;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class MusicManager {
+    private static MediaPlayer currentPlayer;
+
+    public static void play(String resourcePath) {
+        stop();
+
+        URL music = MusicManager.class.getResource(resourcePath);
+        if (music == null) {
+            System.out.println("Fichier audio non trouvé : " + resourcePath);
+            return;
+        }
+        Media media = new Media(music.toExternalForm());
+        currentPlayer = new MediaPlayer(media);
+        currentPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        currentPlayer.setOnReady(() -> currentPlayer.play());
+    }
+
+    public static void stop() {
+        if (currentPlayer != null) {
+            currentPlayer.stop();
+            currentPlayer.dispose();
+            currentPlayer = null;
+        }
+    }
+}
