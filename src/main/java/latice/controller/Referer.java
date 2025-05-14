236bb9d580a89a7c51f60db5f76b2af3e3833a4f
Louis Biard <louisbiard24@gmail.com>
2025-05-14 11:49:49 +0200

    Ajout de shapeTile et colorTile

---
+++ src/main/java/latice/controller/Referer.java
@@ -0,0 +1,17 @@
+package latice.controller;
+
+import javafx.scene.layout.GridPane;
+import latice.model.Color;
+import latice.model.Rack;
+import latice.model.Shape;
+
+public class Referer {
+	
+	public Shape  shapeTile(Rack rack) {
+		//TODO
+	}
+	
+	public Color colorTile(Rack rack) {
+		//TODO
+	}
+}
