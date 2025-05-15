36147dafd630759d037584a311a29fad5eb91482
Louis Biard <louisbiard24@gmail.com>
2025-05-15 09:34:24 +0200

    Switch des trois méthodes précédentes

--- src/main/java/latice/controller/GameViewController.java
+++ src/main/java/latice/controller/GameViewController.java
@@ -315,56 +315,6 @@ public class GameViewController implements EventHandler<MouseEvent>{
 	}
 	
 	
-	private String checkTile(ImageView imageGrid) {
-		Image img = imageGrid.getImage();
-		String urlImage = Tile.url(img);
-		return urlImage;
-	}
-	
-	private Shape checkShape(String url) {
-		String shape = url.substring(137,139);
-		if (shape.equals("fl")){
-			return Shape.FLOWER;
-		}
-		else if (shape.equals("ge")) {
-			return Shape.GECKO;
-		}
-		else if (shape.equals("bi")) {
-			return Shape.BIRD;
-		}
-		else if (shape.equals("tu")) {
-			return Shape.TURTLE;
-		}
-		else if (shape.equals("fe")) {
-			return Shape.FEATHER;
-		}
-		else {
-			return Shape.DOLPHIN;
-		}
-	}
-	
-	private Color checkColor(String url) {
-		String color = url.substring(url.length()-5, url.length()-4);
-		if (color.equals("y")){
-			return Color.YELLOW;
-		}
-		else if (color.equals("r")) {
-			return Color.RED;
-		}
-		else if (color.equals("t")) {
-			return Color.TEAL;
-		}
-		else if (color.equals("n")) {
-			return Color.NAVY;
-		}
-		else if (color.equals("g")) {
-			return Color.GREEN;
-		}
-		else {
-			return Color.MAGENTA;
-		}
-		
-	}
  	
 
 	private void changeTiles(Image imageTile1p1, Image imageTile2p1, Image imageTile3p1, Image imageTile4p1,
